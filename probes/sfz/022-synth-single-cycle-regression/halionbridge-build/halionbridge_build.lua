45d0185793fbb2e945f5b490b1ce5a78bab087c8
B. Zeiss <ben@zeiss.net>
2026-06-13 22:32:36 +0200

    updates

---
+++ probes/sfz/022-synth-single-cycle-regression/halionbridge-build/halionbridge_build.lua
@@ -0,0 +1,4 @@
+return {
+    "000_000_synth_single_cycle_six_regions.lua",
+    "001_001_synth_single_cycle_three_regions_three_velocity_layers.lua",
+}
---
+++ probes/sfz/023-velocity-boundary-diagnostic/halionbridge-build/halionbridge_build.lua
@@ -0,0 +1,3 @@
+return {
+    "000_001_synth_single_cycle_three_regions_three_velocity_layers.lua",
+}
